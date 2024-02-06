6ca4035a8544edc6d2a4402d2eaa355650c2a22a
Frcbm <147309756+Frcbm@users.noreply.github.com>
2024-02-06 17:14:30 +0100

    Add files via upload

--- tasca 3.2/js.js
+++ tasca 3.2/js.js
@@ -1,22 +1,99 @@
-let array = [1,2,2,3,4,4];
-let duplicado = false;
-let dupli = 0;
-for(let i = 0 ; i < array.length ; i++){
-    duplicado = false;
-    for(let j = i + 1 ; j < array.length ; j++){
-        if(array[i] === array[j]){
-            duplicado = true;
-            dupli = array[i];
-        }
-    }
-    if(duplicado){
-        for(let j = 0; j < array.length ; j++){
-            if(array[j] == dupli){
-                array.filter(j);
-            }
-        }
+function mostrarContenido(){
+    let main = document.querySelector("main");
+    let aMostrar = document.getElementById("ejercicio").value;
+    Array.from(main.children).forEach(elemento => {
+        elemento.style.display = "none";
+    });
+
+    switch(aMostrar){
+        case "ejercicio1":
+            eliminarDuplicados();
+            document.getElementById("ej1").style.display = "flex";
+            break;
+        case "ejercicio2":
+            document.getElementById("ej2").style.display = "flex";
+            break;
+        case "ejercicio3":
+            document.getElementById("ej3").style.display = "flex";
+            break;
+        case "ejercicio4":
+            document.getElementById("ej4").style.display = "flex";
+            break;
+        case "ejercicio5":
+            document.getElementById("ej5").style.display= "flex";
+            break;
+        case "ejercicio6":
+            document.getElementById("ej6").style.display= "flex";
     }
 }
-for(let i = 0; i < array.length ; i++){
-    console.log(array[i]);
+function eliminarDuplicados(){
+    
+    let array = [1, 2, 2, 2, 3, 4, 4];
+    let newArray = [];
+    let texto = document.getElementById("resultadoej1");
+    texto.innerHTML = "Primer array " + array +"<br>";
+    for (let i = 0; i < array.length; i++) {
+        let cuenta = array.filter(item => item === array[i]).length;
+
+        if (cuenta === 1) {
+            newArray.push(array[i]);
+        }
+    }
+    array = newArray;
+    texto.innerHTML += "Segundo array " + array;
+}
+function calcularPrecio(){
+    event.preventDefault();
+    let precio = document.getElementById("precio").value;
+    let marca = document.getElementById("marca").value;
+    let texto = document.getElementById("respuesta");
+    let descuento = 0;
+    switch(marca){
+        case "Ford": descuento = 10; break;
+        case "Seat": descuento = 8; break;
+        case "Citroen": descuento = 6; break;
+        case "Audi": descuento = 4; break;
+    }
+    let precioFinal = parseInt(precio - (precio * descuento) / 100);
+
+    texto.innerText = "El coche valdrá " + precioFinal;
+}
+function mayoriaDeEdad(){
+    event.preventDefault();
+
+    let edad = parseInt(document.getElementById("edad").value);
+    let text = document.getElementById("respuestaej3");
+
+    text.innerHTML = edad >= 16 ? "Clica para acceder a la <a href=\"https://www.caib.es/sites/fp/ca/prova_portada/?campa=yes\">web de FP de la CAIB</a>" : "Aun no tienes edad para acceder al portal de FP"; 
+}
+function cantidadLinks(){
+    let texto = document.getElementById("cosasEjercicio4");
+    let google = document.getElementsByClassName("google").length;
+    texto.innerText = "La página tiene " + document.links.length + " enlaces";
+}
+function textoEjercicio5(){
+    let texto = document.getElementById("textoEjercicio5").style.display = "flex";
+}
+function agrandar(){
+    let imagen = document.getElementById("imagen");
+    imagen.style.width.replace("px", "");
+    imagen.style.width = (parseInt(imagen.style.width) + 20) + "px";
+    imagen.style.height.replace("px", "");
+    imagen.style.height = (parseInt(imagen.style.height) + 20) + "px";
+}
+function reducir(){
+    let imagen = document.getElementById("imagen");
+    imagen.style.width.replace("px", "");
+    imagen.style.width = (parseInt(imagen.style.width) - 20) + "px";
+    imagen.style.height.replace("px", "");
+    imagen.style.height = (parseInt(imagen.style.height) - 20) + "px";
+}
+function visibleInvisible(){
+    let imagen = document.getElementById("imagen");
+    if(imagen.style.display == "flex"){
+        imagen.style.display = "none";
+    }else{
+        imagen.style.display = "flex"
+    }
+
 }
