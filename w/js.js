3103131d7c9eba6b227df165574637e9c2b04d0c
Frcbm <147309756+Frcbm@users.noreply.github.com>
2024-03-14 16:32:34 +0100

    Add files via upload

--- w/js.js
+++ w/js.js
@@ -1,7 +1,13 @@
+/*Esta primera función se debe a que a la hora de cambiar el color de fondo
+con un boton tenía problemas porque la primera vez que le daba al boton
+se ponia el color en negro como atributo del body, de esta forma en cambio,
+se pone al cargar,ya que lo llamo con un onload del body, y la primera vez
+que apreto al botón lo cambia de forma correcta*/
 function colorBody(){
     let body = document.getElementsByTagName("body")[0];
     body.style.backgroundColor = "black";
 }
+//Cambio de color de fondo
 function cambiarTema(){
     let body = document.getElementsByTagName("body")[0];
     if(body.style.backgroundColor == "black"){
@@ -10,6 +16,9 @@ function cambiarTema(){
         body.style.backgroundColor = "black";
     }
 }
+
+//Estructura de carruseles, para el boton siguiente y para el de anterior
+//Hay carruseles en 3 páginas y todos utilizan estas funciones
 var i = 0;
 function avanzar(){    
     let article = document.getElementsByClassName("parrafo");
@@ -29,12 +38,20 @@ function retroceder(){
     }
     article[i].style.display = "flex";
 }
+//Esto se utilizará mas adelante
 function generarID(){
     return parseInt(Math.random() * 1000 + 1);
 }
-/*Validació formulari Sorà*/ 
+
+/*Validación formulario Sorà*/ 
 function validarDatos(){
-    
+    /*Nuestra idea para generar un carnet de socio pasaba por permitir
+    añadir una foto, pero la única forma de que funcionara fue con un método
+    que encontramos por internet que no acababamos de entender como funciona.
+    Hemos decidido dejarlo aquí como comentario, asi como en el html 
+    "suscripcion.html" y la regex que lo validaba, pero no utilizarlo.
+    En su lugar hemos usado el logo del equipo como imagen generica para
+    todos los carnets.*/
     let nombre = document.getElementById("nombre").value;
     let apellido = document.getElementById("apellido").value;
     nombre = nombre.toUpperCase();
@@ -114,6 +131,8 @@ function crearCarnet() {
     document.getElementById("identificador").textContent = "Socio nº: 000" + generarID();
 }
 
+//En la página jugadores.html se muestra un texto si colocas el ratón
+//encima de la imagen y desaparecera al salir.
 function mostrarText() {
     let tooltip = document.getElementsByClassName("oculto");
     
@@ -122,9 +141,11 @@ function mostrarText() {
     let tooltip = document.getElementsByClassName("oculto");
     tooltip[i].style.visibility = "hidden";
 }
+
+
 function pagament(){    
     let opcio = document.getElementById("tarjeta").value;
-    let form = document.getElementById("formulario");    
+    let form = document.getElementById("form");    
     if(document.getElementById("ele") != null){
         reset();
     }    let but = document.createElement("button");
@@ -134,7 +155,8 @@ function pagament(){
     nuevoElemento.setAttribute("id","ele");
     nuevoElemento.setAttribute("type","text");
     let lbl = document.createElement("label");
-    lbl.setAttribute("for","ele");    switch(opcio){
+    lbl.setAttribute("for","ele");    
+    switch(opcio){
         case "visa":
             lbl.textContent = "Numero de tarjeta: ";
             form.appendChild(lbl);
@@ -160,7 +182,9 @@ function pagament(){
             form.appendChild(but);
             break;
     }
-}function reset(){    if(document.getElementById("ele") != null){
+}function reset(){
+    let form = document.getElementById("form");    
+    if(document.getElementById("ele") != null){
         form.removeChild(form.lastChild);
         form.removeChild(form.lastChild);
         form.removeChild(form.lastChild);
@@ -169,34 +193,204 @@ function pagament(){
 /*Validació formulari Barbón*/
 function validar(){
     let nom = document.getElementById("nom").value;
-    if(nom.length < 8){
+    nom = nom.toUpperCase();
+    if(!/^[A-ZÁÉÍÓÚÀÈÒ][A-ZÁÉÍÓÚÀÈÒ ]+$/.test(nom)){
         alert("Nom no vàlid");
         return false;
-    }    let llin = document.getElementById("llin").value;
-    const patternLlin = /^([a-zA-ZáéíóúüÁÉÍÓÚÜñÑ]{2,60}[\,\-\.]{0,1}[\s]{0,1}){1,3}$/;
+    }    
+    let llin = document.getElementById("llin").value;
+    llin = llin.toUpperCase();
+    const patternLlin = !/^[A-ZÁÉÍÓÚÀÈÒ][A-ZÁÉÍÓÚÀÈÒ ]+$/;
     if(!llin.test(patternLlin)){
         alert("Llinatges no vàlids");
         return false;
-    }    let dir = document.getElementById("dir").value;
+    }    
+    let dir = document.getElementById("dir").value;
     const patternCall= /^(calle|avenida|carretera|plaza|paseo|camino)\s[A-Za-z0-9\s\.,#-]+$/;
     if(!dir.test(patternCall)){
         alert("Direcció no vàlida");
         return false;
-    }    let mail = document.getElementById("email").value;
+    }    
+    let mail = document.getElementById("email").value;
     const patternMail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     if(!mail.test(patternMail)){
         alert("Direcció de correu electrònic no vàlida");
         return false;
-    }    let tlf = document.getElementById("tlf").value;
+    }    
+    let tlf = document.getElementById("tlf").value;
     const patternTlf = /^6[0-9]{8}$/;
     if(!tlf.test(patternTlf)){
         alert("Telèfon no vàlid");
         return false;
-    }    return true;
+    }    
+    return true;
 }
-function agrandarFotos(){
+/*He hecho 2 funciones para cada una de las 4 fotos con clase fotoEvento,
+para poder hacer que se agranden al entrar el raton y se reduzcan al salir*/
+//foto1
+function agrandarFotos1(){
     let fotos = document.getElementsByClassName("fotosEvento");
-
-    onmouseenter.fotos[0] = fotos[0].style.width = "250px";
-    onmouseenter.fotos[0] = fotos[0].style.height = "250px";
+    for(let i = 0 ; i < fotos.length ; i++){
+        fotos[i].style.display = "none";
+    }
+    let foto = document.getElementById("evento1");
+    foto.style.display = "flex";
+    foto.style.width= "100%";
+    foto.style.height = "80%";
 }
+function reducirFotos1(){
+    let fotos = document.getElementsByClassName("fotosEvento");
+    for(let i = 0 ; i < fotos.length ; i++){
+        fotos[i].style.display = "flex";
+    }
+    let foto = document.getElementById("evento1");
+    foto.style.width= "20%";
+    foto.style.height = "20%";
+}
+//foto2
+function agrandarFotos2(){
+    let fotos = document.getElementsByClassName("fotosEvento");
+    for(let i = 0 ; i < fotos.length ; i++){
+        fotos[i].style.display = "none";
+    }
+    let foto = document.getElementById("evento2");
+    foto.style.display = "flex";
+    foto.style.width= "100%";
+    foto.style.height = "80%";
+}
+function reducirFotos2(){
+    let fotos = document.getElementsByClassName("fotosEvento");
+    for(let i = 0 ; i < fotos.length ; i++){
+        fotos[i].style.display = "flex";
+    }
+    let foto = document.getElementById("evento2");
+    foto.style.width= "20%";
+    foto.style.height = "20%";
+}
+//foto 3
+function agrandarFotos3(){
+    let fotos = document.getElementsByClassName("fotosEvento");
+    for(let i = 0 ; i < fotos.length ; i++){
+        fotos[i].style.display = "none";
+    }
+    let foto = document.getElementById("evento3");
+    foto.style.display = "flex";
+    foto.style.width= "100%";
+    foto.style.height = "80%";
+}
+function reducirFotos3(){
+    let fotos = document.getElementsByClassName("fotosEvento");
+    for(let i = 0 ; i < fotos.length ; i++){
+        fotos[i].style.display = "flex";
+    }
+    let foto = document.getElementById("evento3");
+    foto.style.width= "20%";
+    foto.style.height = "20%";
+}
+//foto 4
+function agrandarFotos4(){
+    let fotos = document.getElementsByClassName("fotosEvento");
+    for(let i = 0 ; i < fotos.length ; i++){
+        fotos[i].style.display = "none";
+    }
+    let foto = document.getElementById("evento4");
+    foto.style.display = "flex";
+    foto.style.width= "100%";
+    foto.style.height = "80%";
+}
+function reducirFotos4(){
+    let fotos = document.getElementsByClassName("fotosEvento");
+    for(let i = 0 ; i < fotos.length ; i++){
+        fotos[i].style.display = "flex";
+    }
+    let foto = document.getElementById("evento4");
+    foto.style.width= "20%";
+    foto.style.height = "20%";
+}
+var media = 0;
+var count = 0;
+var acumulado = 0;
+function puntuaPagina(){
+    count++;
+
+    let checkbox1 = document.getElementById("1estrella");
+    let checkbox2 = document.getElementById("2estrella");
+    let checkbox3 = document.getElementById("3estrella");
+    let checkbox4 = document.getElementById("4estrella");
+    let checkbox5 = document.getElementById("5estrella");
+    let valor1 = +checkbox1.value;
+    let valor2 = +checkbox2.value;
+    let valor3 = +checkbox3.value;
+    let valor4 = +checkbox4.value;
+    let valor5 = +checkbox5.value;
+        
+        if(checkbox1.checked != false){
+            acumulado = acumulado + valor1;
+            media = (acumulado/count).toFixed(2);
+        }
+        if(checkbox2.checked != false){
+            acumulado = acumulado + valor2;
+            media = (acumulado/count).toFixed(2);
+        }
+        if(checkbox3.checked != false){
+            acumulado = acumulado + valor3;
+            media = (acumulado/count).toFixed(2);
+        }
+        if(checkbox4.checked != false){
+            acumulado = acumulado + valor4;
+            media = (acumulado/count).toFixed(2);
+        }
+        if(checkbox5.checked != false){
+            acumulado = acumulado + valor5;
+            media = (acumulado/count).toFixed(2);
+        }
+
+        document.getElementById("media").textContent = media;
+    }
+    function desmarca(){
+        let checkbox1 = document.getElementById("1estrella");
+        let checkbox2 = document.getElementById("2estrella");
+        let checkbox3 = document.getElementById("3estrella");
+        let checkbox4 = document.getElementById("4estrella");
+        let checkbox5 = document.getElementById("5estrella");
+        checkbox1.onclick = function(){
+            if(checkbox1.checked != false){
+                checkbox2.checked =null;
+                checkbox3.checked =null;
+                checkbox4.checked =null;
+                checkbox5.checked =null;
+            }
+        }
+        checkbox2.onclick = function(){
+            if(checkbox2.checked != false){
+                checkbox1.checked=null;
+                checkbox3.checked =null;
+                checkbox4.checked =null;
+                checkbox5.checked =null;
+            }
+        }
+        checkbox3.onclick = function(){
+            if(checkbox3.checked != false){
+                checkbox1.checked=null;
+                checkbox2.checked =null;
+                checkbox4.checked =null;
+                checkbox5.checked =null;
+            }
+        }
+        checkbox4.onclick = function(){
+            if(checkbox4.checked != false){
+                checkbox1.checked=null;
+                checkbox3.checked =null;
+                checkbox2.checked =null;
+                checkbox5.checked =null;
+            }
+        }
+        checkbox5.onclick = function(){
+            if(checkbox5.checked != false){
+                checkbox1.checked=null;
+                checkbox3.checked =null;
+                checkbox4.checked =null;
+                checkbox2.checked =null;
+            }
+        }
+    }
